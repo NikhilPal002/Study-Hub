bc2b6edac5ad3552e2f0aa6a6ab7d271bc6272cf
Nikhil <nikhilpal2017@gmail.com>
2023-07-29 01:51:16 +0530

    resitering the user in db correctly

--- routes/registerRoute.js
+++ routes/registerRoute.js
@@ -3,11 +3,34 @@ const router = express.Router();
 const User = require('../models/register');
 const { body, validationResult } = require('express-validator');
 
-router.get('/register', (req, res) => {
-  console.log(req.body);
-  const user = User(req.body);
-  user.save();
-  res.send(req.body);
-})
+router.post('/register',
+  body('username').isLength({ min: 3, max: 20 }),
+  body('phoneNumber').isLength({ min: 10, max: 10 }),
+  body('email').isEmail().normalizeEmail(),
+  body('password').isLength({ min: 8 }
+  ), (req, res) => {
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+      // Save the user to the database (assuming you have a User model defined)
+      const newUser = new User({
+        username: req.body.username,
+        phoneNumber: req.body.phoneNumber,
+        email: req.body.email,
+        password: req.body.password,
+      });
+
+      newUser.save()
+        .then(() => {
+          res.send(`User ${req.body.username} registered successfully!`);
+        })
+        .catch((err) => {
+          console.error('Error saving user:', err);
+          res.status(500).send('Error registering user.');
+        });
+    } else {
+      res.status(422).json({ errors: result.array() });
+    }
+
+  })
 
 module.exports = router;
