4cec01d9f9bae81e26125a6806bff10ce51f6d2e
Nikhil <nikhilpal2017@gmail.com>
2023-07-29 11:12:23 +0530

    hashed the password using bcryptjs, working properly

--- routes/registerRoute.js
+++ routes/registerRoute.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/register');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require("bcrypt");
+
 
 router.post('/register',
   body('username').isLength({ min: 3, max: 20 }),
@@ -11,26 +13,36 @@ router.post('/register',
   ), (req, res) => {
     const result = validationResult(req);
     if (result.isEmpty()) {
-      // Save the user to the database (assuming you have a User model defined)
-      const newUser = new User({
-        username: req.body.username,
-        phoneNumber: req.body.phoneNumber,
-        email: req.body.email,
-        password: req.body.password,
-      });
+      bcrypt.hash(req.body.password, 10 )
+      .then((hashedPassword) => {
+        const newUser = new User({
+          username: req.body.username,
+          phoneNumber: req.body.phoneNumber,
+          email: req.body.email,
+          password: hashedPassword,
+        });
 
-      newUser.save()
-        .then(() => {
-          res.send(`User ${req.body.username} registered successfully!`);
+        newUser.save()
+        .then((result) => {
+          res.status(201).send({
+            message: `${req.body.username} Registered Successfully`,
+            result,
+          });
         })
         .catch((err) => {
           console.error('Error saving user:', err);
-          res.status(500).send('Error registering user.');
+          res.status(500).send('Error registering user, please enter a valid email',err);
         });
+      })
+      .catch((err) => {
+        res.status(500).send({
+          message: "Error registering user", err
+        });
+      });
     } else {
       res.status(422).json({ errors: result.array() });
     }
 
-  })
+  });
 
 module.exports = router;
